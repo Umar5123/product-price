868c60e58846261f3a2ad6cd36aeb3b11da1cb72
umar5123 <umar03408684954@gmail.com>
2024-08-07 20:16:46 +0500

    product price fixed

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#select').change(function(){
+        var selectedDiscount = $('#select option:selected').attr("discount");
+        var discountPercent = parseFloat(selectedDiscount);
+
+        var productPrice = $('#select option:selected').attr("price");
+
+        $('#discount span').text(discountPercent + '%');
+        $('#produntPrice span').text(productPrice);
+
+        var discountAmount = (discountPercent / 100) * productPrice;
+        var totalPrice = productPrice - discountAmount;
+
+        $('#totalPrice span').text(totalPrice);
+        $('#discount span').text(discountPercent);
+    });
+});
